53ce82fd34a2429aad4ebc20094a977b5186ea8d
Shivam Singh <osg82b@gmail.com>
2024-02-24 12:17:12 +0530

    added bassic json files

---
+++ website/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./nodeapp/models/fetch-model')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://localhost:27017/data")
+
+app.get('/getUsers', (req, res) => {
+    UserModel.find()
+    .then(users => res.json(users))
+    .catch(err => res.json)
+})
+
+app.listen(5005, () => {
+    console.log("Server is running")
+})
